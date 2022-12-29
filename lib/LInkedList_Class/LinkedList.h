3c5db7c73848cc216a1f6055e45bb429dc571fd2
Mohamed Qushta <mohamed.qushta.123@gmail.com>
2022-12-29 23:36:03 +0200

    Linked list node done

--- lib/LInkedList_Class/LinkedList.h
+++ lib/LInkedList_Class/LinkedList.h
@@ -1,10 +1,16 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
-
+#include "../Client_Class/Client.h"
 class LinkedListNode
 {
 public:
-
+    Client data;
+    LinkedListNode * next;
+    LinkedListNode(Client d)
+    {
+        data = d;
+        next = NULL;    
+    }
 };
 
 
