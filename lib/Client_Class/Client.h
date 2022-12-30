f164da1893b52b7269d11d7c32dd54a6bb103e29
Michael-2003 <107130078+Michael-2003@users.noreply.github.com>
2022-12-30 21:49:29 +0200

    Update Client.h

--- lib/Client_Class/Client.h
+++ lib/Client_Class/Client.h
@@ -6,21 +6,66 @@ using namespace std;
 
 class Client
 {
-public:
-
-
-
-// setter
-void setClientName(string name) { clientName = name;}
-void setClientEmail(string email) {clientEmail = email;}
-
-
-// getter
-string getClientName(){return clientName;}
 
 private:
 
-string clientName, clientEmail;
+string clientName, clientEmail, clientPhone, ClientAddress, clientPassword;
+int clientID;
+
+// <Transaction array>
+
+public:
+
+// constructor
+
+Client(){
+clientName=""; 
+clientEmail=""; 
+clientPhone=""; 
+ClientAddress=""; 
+clientPassword="";
+clientID=0;
+// Transaction
+}
+
+Client(string n, string e, string p, string a)
+{
+clientName = n;
+clientEmail = e;
+clientPhone = p;
+ClientAddress = a;
+}
+
+// setters
+
+void setClientName(string name) { clientName = name;}
+void setClientEmail(string email) {clientEmail = email;}
+void setclientPhone(string phone) {clientPhone = phone;}
+void setclientAddress(string address) {ClientAddress = address;}
+void setclientPassword(string password) {clientPassword = password;}
+void setclientID(int id) {clientID = id;}
+
+// getters
+
+string getClientName(){return clientName;}
+string getClientEmail() {return clientEmail;}
+string getclientPhone() {return clientPhone;}
+string getclientAddress() {return ClientAddress;}
+string getclientPassword() {return clientPassword;}
+int getclientID() {return clientID;}
+
+
+// Function that print client info
+
+void print_client_info(){
+
+cout<<"Name: "<<clientName<<endl;
+cout<<"ID: "<<clientID<<endl;
+cout<<"Email: "<<clientEmail<<endl;
+cout<<"Phone: "<<clientPhone<<endl;
+cout<<"Address: "<<ClientAddress<<endl;
+
+}
 
 };
 
