f9e0459ad46d02d4743ff8f24975a07e86a46f41
Mohamed Abdelfattah <mohamed@gmail.com>
2022-12-30 16:40:15 +0200

    Transaction.cpp & Transaction.h

--- lib/Transaction_Class/Transaction.cpp
+++ lib/Transaction_Class/Transaction.cpp
@@ -1,1 +1,50 @@
 #include "Transaction.h"
+#include <iostream>
+using namespace std;
+// Default constructor imblementation
+transaction::transaction()
+{
+    transactionID = 0;
+    transactionName = "";
+    transactionDate = "";
+}
+ // Parameterized constructor imblementation
+ transaction::transaction(int id)
+ {
+     transactionID = id;
+     transactionName = "Withdraw";
+     transactionDate = "14/Dec/2022";
+ }
+// setters imblementation
+void transaction::setId(int id)
+{
+    transactionID = id;
+}
+void transaction::setName(string name)
+{
+    transactionName = name;
+}
+void transaction::setDate(string date)
+{
+    transactionDate = date;
+}
+// getters imblementation
+int transaction::getId()
+{
+    return transactionID;
+}
+string transaction::getName()
+{
+    return transactionName;
+}
+string transaction::getDate()
+{
+    return transactionDate;
+}
+// print function imblementation
+void transaction::printTransactionInfo()
+{
+    cout << "Transaction Id: " << getId() << endl;
+    cout << "Transaction Name: " << getName() << endl;
+    cout << "Transaction Date: " << getDate() << endl;
+}
--- lib/Transaction_Class/Transaction.h
+++ lib/Transaction_Class/Transaction.h
@@ -1,8 +1,31 @@
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
+#include <iostream>
+using namespace std;
 
+class transaction
+{
+private:
+    // attributes
+    int transactionID;
+    string transactionName;
+    string transactionDate;
 
-
-
+public:
+    // Default constructor imblementation
+    transaction();
+    // Parameterized constructor imblementation
+     transaction(int id);
+    // setters
+    void setId(int id);
+    void setName(string name);
+    void setDate(string date);
+    // getters
+    int getId();
+    string getName();
+    string getDate();
+    // print the transaction info
+    void printTransactionInfo();
+};
 
 #endif
