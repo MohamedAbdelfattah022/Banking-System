1c5e37207b0e9f2372146f13e9a35e413a335b5e
Morshedy22 <o.ayman2149@nu.edu.eg>
2023-01-04 10:49:57 +0200

    Update main

--- testmain.cpp
+++ testmain.cpp
@@ -4,7 +4,7 @@
 // include classes we Implemeneted
 #include "lib/Client_Class/Client.h"
 #include "lib/LInkedList_Class/LinkedList.h" 
-#include "lib/Transaction_Class/Transaction.cpp"
+#include "lib/Transaction_Class/Transaction.h"
 
 // include most popular lib.
 #include <bits/stdc++.h>
@@ -15,8 +15,21 @@ using namespace std;
 
 int main()
 {
+    Client c("Ashraf", "Ashraf@nu.edu.eg", "0100", "Alex", 21100);
+    Transaction t1(10), t2(20), t3(30), t4(40), t5(50), t6(60), t7(70);
 
+    c.setTransaction(t1);
+    c.setTransaction(t2);
+    c.setTransaction(t4);
+    c.setTransaction(t5);
+    c.setTransaction(t6);
+    c.setTransaction(t7);
 
+    c.print_client_info();
+
+    cout << "\n\n";
+
+    c.getClientTransaction();
 
 
 }
